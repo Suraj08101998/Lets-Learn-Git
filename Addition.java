ba78b555f691607c7cb82ec33a961e82c6d66a21
Suraj D. Chikate <s.chikate@somaiya.edu>
2021-09-28 11:55:19 +0530

    Adding Addition.java

---
+++ Addition.java
@@ -0,0 +1,19 @@
+
+public class Addition {
+
+	
+	static int a=5;
+	static int b=10;
+	
+	public static void add(int l, int m)
+	{
+		int sum=l+m;
+	}
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+	
+		add(a,b);
+	}
+
+}
